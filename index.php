36a1aa905fbae868c7dbdf4566ba2c3d110a458f
SeptinaAliza <125982066+SeptinaAliza@users.noreply.github.com>
2023-02-26 18:30:19 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+  
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Septina Aliza.</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+          
+            <div class="logo"><a href=''>Septina Aliza</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href ="#Home">Home</a></li>
+                    <li><a href ="#profile">profile</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+
+    </section>
+     <!--untuk home-->
+        <section id="Home">
+            <img src="https://img.freepik.com/free-vector/programming-concept-illustration_114360-1351.jpg?w=740&t=st=1677299278~exp=1677299878~hmac=54524e91604ecacc106b208e485338eb6c5c379a2750b2010f8853226fc50c08"/>
+            <div class="kolom">
+                <p> Jangan pernah takut memulai sesuatu </p> 
+                <p class="deskripsi"><h1> Studi Independen Bersertifikat Full Stack Web Developer GITS </h1></p>
+                <p>life is inherently risky. There is only one big ris you should avoid at all costs, and that is the risk of doing nothing</p>
+            </section>
+            <!--untuk profile-->
+            <section id="profile">
+                <div class="tengah">
+                    <div class="kolom">
+                        <p class="deskripsi"><h1>My Profile</h1></p>
+                    </div>
+
+                    <div class="profile">
+                        <div class="kartu-profile">
+
+                            <img src="https://i.pinimg.com/originals/96/45/80/964580429e1c0754c684b275c3a8b56b.jpg"/>
+                            <p>Septina Aliza</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+          </div>
+             <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                    <div class="footer-section">
+                        <h3>About</h3>
+                        <p>Hello guys, introduce my name is septina aliza, usually called septina or liza. I come from Gelogor Village, Kec. Kediri Kab. LOBAR.
+                        MATARAM UNIVERSITY OF TECHNOLOGY Student
+                        Information Systems Major.</p>
+                    </div>
+                     <div class="footer-section">
+                        <h3>Contact</h3>
+                        <p>Jln. TGH Abdul Karim, Kediri, Kabupaten Lombok Barat, Nusa Tenggara Barat</p>
+                        <p>Kode Pos: 83362</p>
+                    </div>
+                      <div class="footer-section">
+                        <h3>Social Media</h3>
+                        <p><b>WhatsApp: </b>087765110934(sang pencapai)</p>
+                    </div>
+                </div>
+             </div>
+       </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2023. <b>Septina Aliza.</b> All  Rights Reserved.
+        </div>
+    </div>
+</body>
+</html>
